b9932518038f8c4ab42e55b45d9386486b75adf3
JinseonLee <memilmuk82@gmail.com>
2022-12-02 12:19:25 +0900

    noom_v0.8

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,40 +1,18 @@
-const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("#message");
-const nickForm = document.querySelector("#nick");
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = io(); // io() -> socket.io를 실행하고 있는 서버 탐색
+// socket.io가 웹 소켓을 이용해 기능 수행
 
-function makeMessage(type, payload){
-    const msg = { type, payload };
-    return JSON.stringify(msg);
-}
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
 
-socket.addEventListener("open", () => {
-    console.log("Connect to Server")
-});
-
-socket.addEventListener("message", (message) => {
-    const li = document.createElement("li");
-    li.innerText = message.data;
-    messageList.append(li);
-});
-
-socket.addEventListener("close", () => {
-    console.log("Disconnected from Server")
-});
-
-function handleSubmit(event){
+function handleRoomSubmit(event){ // input에 입력된 값을 읽고, 이를 소켓을 통해 서버로 전송
     event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(makeMessage("new_message", input.value));
+    const input = form.querySelector("input");
+    socket.emit("enter_room", input.value, () => {
+        // 첫번째 인자: 이벤트명, 두번째 인자: 서버에 전쇼ㅗㅇ할 데이터, 세번째 인자: 익명 함수
+        console.log("server is done!");
+    });
+    // socket.emit 메소드 -> 이벤트 발생 / JSON객체를 통한 문자열 변환을 하지 않아도 원활하게 데이터 전송이 이루어짐.    
     input.value = "";
 };
 
-function handleNickSubmit(event){
-    event.preventDefault();
-    const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname", input.value));
-    input.value = "";
-};
-
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+form.addEventListener("submit", handleRoomSubmit); // submit 이벤트 발생 -> handleRoomSubmit
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from 'express';
 
 const app = express();
@@ -10,29 +10,22 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log("Listening on http://localhost:3000");
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpserver = http.createServer(app);
+// Node.js에 기본으로 내장된 HTTP패키지를 사용해서 express.js로 만든 서버 애플리케이션 제공
+// HTTP서버: httpserver, 웹소켓서버: wsServer
+const wsServer = SocketIO(httpserver); // wsServer를 만들기 위해 SocketIO에 httpserver를 넘겨줌
 
-const sockets = [];
-
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anonymous"
-    console.log("Connected to Browser");
-    socket.on("close", () => console.log("Disconnected from Browser"));
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch(message.type){
-            case "new_message":
-                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}:${message.payload}`));
-                break;
-            case "nickname":
-                socket["nickname"] = message.payload;
-                break;
-        };
-        
+wsServer.on("connection", (socket) => {
+    socket.on("enter_room", (roomName, done) => {
+        // socket.on -> 이벤트 핸들링 메소드
+        // 첫번째 인자 -> 이벤트 이름, 두번째 인자 -> 이벤트 핸들러 함수
+        // 매개변수 roonName -> 프론트엔드에서 emit메소드를 통해 보낸 객체가 전달됨.
+        console.log(roomName);
+        setTimeout(() => {
+            done();
+        }, 5000);
     });
-});
+});    
 
-server.listen(3000, handleListen);
+const handleListen = () => console.log("Listening on http://localhost:3000");
+httpserver.listen(3000, handleListen);
